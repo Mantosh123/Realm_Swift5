d1d554a2fd34a76f67a97ea6aaeb9fb1dc95e162
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-11-17 11:47:36 +0530

    Add My Contact project and add .gitignore file

---
+++ MyContacts/MyContactsTests/MyContactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsTests.swift
+//  MyContactsTests
+//
+//  Created by Mantosh Kumar on 17/11/24.
+//
+
+import Testing
+@testable import MyContacts
+
+struct MyContactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
